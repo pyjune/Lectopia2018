e976092b7b473c896d8b587a0ee3f928c20542c9
pyjune <pyjune@naver.com>
2018-07-16 11:49:55 +0900

    Create count.c

---
+++ day1/count.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int T;
+
+	freopen("Text.txt", "r", stdin);
+	scanf("%d", &T);
+	for (int tc = 1; tc <= T; tc++)
+	{
+		int N;
+		int cnt[10] = { 0 };
+		scanf("%d", &N);
+		for (int i = 0; i < N; i++)
+		{
+			int num;
+			scanf("%1d", &num);
+			cnt[num]++;		// 숫자의 개수를 기록..
+		}
+		// 최대값의 인덱스 찾기
+		int maxIdx = 0;
+		for (int i = 1; i < 10; i++)
+		{
+			if (cnt[i] > cnt[maxIdx])
+				maxIdx = i;
+		}
+		printf("#%d %d %d\n", tc, maxIdx, cnt[maxIdx]);
+	}
+
+}
