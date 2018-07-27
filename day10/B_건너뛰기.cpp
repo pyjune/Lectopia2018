ce25c76b71276235a4b946ee395483eb319da491
pyjune <pyjune@naver.com>
2018-07-27 12:54:20 +0900

    Update B_건너뛰기.cpp

--- day10/B_건너뛰기.cpp
+++ day10/B_건너뛰기.cpp
@@ -20,25 +20,18 @@ int main(void)
 				scanf("%d", &map[i][j]);
 			}
 		}
+		d[1][1] = 1;
 		for (int i = 1; i <= N; i++)
 		{
 			for (int j = 1; j <= M; j++)
 			{
-				if (i == 1 && j == 1)
-					d[i][j] = 1;
-				else
-				{
-					for (int l = 1; l < i; l++)
+					for (int l = 1; l <=3; l++)
 					{
-						if (map[i-l][j] == l)
+						if (i - l > 0 && map[i - l][j] == l)
 							d[i][j] += d[i-l][j];
-					}
-					for (int l = 1; l < j; l++)
-					{
-						if (map[i][j - l] == l)
+						if (j - l > 0 && map[i][j - l] == l)
 							d[i][j] += d[i][j - l];
 					}
-				}
 			}
 		}
 		printf("#%d %d\n", tc, d[N][M]);
